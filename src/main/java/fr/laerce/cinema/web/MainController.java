3f744b0cd4355c794ac1c24bc190e0049c496743
karlmenino <karl.menino@gmail.com>
2018-11-30 13:14:52 +0100

    1er exercice du prof

--- src/main/java/fr/laerce/cinema/web/MainController.java
+++ src/main/java/fr/laerce/cinema/web/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //pour dire a springboot qu'il est un controller web on écrit cette phrase
 @Controller
@@ -26,5 +28,24 @@ public class MainController {
         //on return la chaine string index de façon à ouvrir index.html
         return "index";
     }
+    ///////////////////////////////////////////////////////////////
+    //on map vers film donc dans l'url on ecrira /film?id=1
+//    @GetMapping("/film")
+    //on recupere le parametre id de l'url grace à raquestparam
+//    public String detail(Model m, @RequestParam("id") String id){
+    ////////////////////////////////////////////////////////////////
+    @GetMapping("/film/{id}")
+    //on recupere id grace à pathvariable
+    public String detail(Model m, @PathVariable("id") String id){
+        Integer idFilm = Integer.parseInt (id);
+        m.addAttribute ("film", filmsDao.getById (idFilm));
+        return"detail";
+    }
+    @GetMapping("/affiche/{id}")
+    public String affiche(Model M,@PathVariable("id") String id){
+
+        return "affiche";
+    }
+
 
 }
